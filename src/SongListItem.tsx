b95c1d514e674a39112807d23a8cef47ad6944bf
Johnnybar <johnnybareket@gmail.com>
2021-12-15 16:52:37 +0100

    feature/api-component - modifies SongListItem to fit get response

--- src/SongListItem.tsx
+++ src/SongListItem.tsx
@@ -8,8 +8,9 @@ interface Props {
 export const SongListItem: React.FC<Props> = ({ song, addToLiked }) => {
   return (
     <li>
-      <span style={{ color: song.liked ? "blue" : "red" }}>{song.title}</span>
-      <img src={song.imageUrl} alt="" />
+      <span style={{ color: song.liked ? "blue" : "red" }}>{song.name}</span>
+      <span>{song.artist_name}</span>
+      <img src={song.cover_image_path} alt="" />
 
       <button
         onClick={() => {
