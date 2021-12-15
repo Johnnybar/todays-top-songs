0da7e4805458cb2aae1c49e9b70f8aac22cd1624
Johnnybar <johnnybareket@gmail.com>
2021-12-15 16:51:49 +0100

    feature/api-component - modularize app - requests moved to SongList

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { SongListItem } from "./SongListItem";
+import { SongList } from "./SongList";
 
-const initialSongs: Song[] = [
-  {
-    title: "testsong1",
-    imageUrl:
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMusical_note&psig=AOvVaw2wTMTKrDsbB6orGiH5wI7D&ust=1639654295633000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCor7Ta5fQCFQAAAAAdAAAAABAH",
-    liked: false,
-  },
-  {
-    title: "testsong2",
-    imageUrl:
-      "https://image.shutterstock.com/image-vector/music-notes-song-melody-tune-260nw-701307613.jpg",
-    liked: false,
-  },
-];
 function App() {
-  const [songs, setLikedSong] = useState(initialSongs);
-
-  const addToLiked = (selectedSong: Song) => {
-    const newSongs = songs.map((song) => {
-      if (song === selectedSong) {
-        return {
-          ...song,
-          liked: !song.liked,
-        };
-      }
-      return song;
-    });
-    setLikedSong(newSongs);
-  };
   return (
     <ul>
-      <SongListItem song={songs[0]} addToLiked={addToLiked} />
-      <SongListItem song={songs[1]} addToLiked={addToLiked} />
+      <SongList />
     </ul>
   );
 }
