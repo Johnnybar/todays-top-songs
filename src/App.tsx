f2b9e3180ec53367d0348eb4db2df136212e1991
Johnnybar <johnnybareket@gmail.com>
2021-12-16 10:50:25 +0100

    feature/ui-component - adds footer, header, initial scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { SongList } from "./SongList";
 
 function App() {
   return (
     <>
+      <header className="loudly-header">
+        <div className="container">
+          <div className="row">
+            <h1 className="col-sm-12 loudly-header__heading">Loudly App</h1>
+          </div>
+        </div>
+      </header>
+
       <SongList />
+      <footer className="loudly-footer">
+        <div className="container">
+          <div className="row">
+            <h4 className="col-sm-12 loudly-footer__text">
+              Loudly: AI Powered, Royalty Free Music Solutions
+            </h4>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
